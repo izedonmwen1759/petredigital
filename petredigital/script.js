709bab55f6af8fff73dac0c4758aaaef431b7a9e
izedonmwen1759 <104003846+izedonmwen1759@users.noreply.github.com>
2024-02-20 00:20:46 +0100

    Add files via upload

---
+++ petredigital/script.js
@@ -0,0 +1,23 @@
+const socket = io('https://ellup-server.onrender.com/')
+//const socket = io('http://localhost:10001/')
+const posthere = document.getElementById('postHere')
+window.addEventListener('load',()=>{
+    var today=new Date(); //Today's Date
+    document.getElementById('year').textContent=today.getFullYear()
+    socket.emit('open', '')
+})
+socket.on('posts',(data)=>{
+    
+    var ht = ''
+    data.sort().reverse()
+    for (let i = 0; i < data.length; i++) {     
+       ht += '<div class="col-lg-4 "><div class="card bg-transparent m-1 border border-primary colored_card" style="width:22rem;"><img src="'+data[i].image+'" class="card-img-top img-responsive"/><div class=""><h3>'+data[i].text+'</h3></div><div class="row"><div class="col"><em>'+data[i].dateTime+'</em></div><div class="col"><span class="text-danger" onclick="deletePost()">Delete</span></div></div><a class="btn btn-sm btn-primary text-white" href="'+data[i].url+'" target="_blank" onclick="countCLick(\''+data[i].id+'\')">Click Here</a></div></div>'
+    }
+   posthere.innerHTML=ht
+})
+function countCLick(id){
+    const data = {
+        id:id
+    }
+    socket.emit('count', data)
+}
