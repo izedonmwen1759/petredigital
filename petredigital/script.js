55ce3474544a6535e0529786399cf1ae8cbbb026
izedonmwen1759 <104003846+izedonmwen1759@users.noreply.github.com>
2024-03-03 18:14:34 +0100

    Update script.js

--- petredigital/script.js
+++ petredigital/script.js
@@ -10,7 +10,7 @@ socket.on('posts',(data)=>{
     var ht = ''
     data.sort().reverse()
     for (let i = 0; i < data.length; i++) {     
-       ht += '<div class="col-lg-4 "><div class="card bg-transparent m-1 border border-primary colored_card" style="width:22rem;"><img src="'+data[i].image+'" class="card-img-top img-responsive"/><div class=""><h3>'+data[i].text+'</h3></div><div class="row"><div class="col"><em>'+data[i].dateTime+'</em></div></div><a class="btn btn-sm btn-primary text-white" href="'+data[i].url+'" target="_blank" onclick="countCLick(\''+data[i].id+'\')">Click Here</a></div></div>'
+       ht += '<div class="col-lg-4 "><div class="card bg-transparent m-1 border border-primary colored_card" style="width:22rem;"><img src="'+data[i].image+'" class="card-img-top img-responsive"/><div class=""><h3>'+data[i].text+'</h3></div><a class="btn btn-sm btn-primary text-white" href="'+data[i].url+'" target="_blank" onclick="countCLick(\''+data[i].id+'\')">Click Here</a></div></div>'
     }
    posthere.innerHTML=ht
 })
