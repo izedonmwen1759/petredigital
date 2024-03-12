1f7679dbe5373bfa1566d7819d44067ba8bb8151
izedonmwen1759 <104003846+izedonmwen1759@users.noreply.github.com>
2024-03-12 20:26:55 +0100

    Update script.js

--- petredigital/script.js
+++ petredigital/script.js
@@ -1,22 +1,15 @@
-const socket = io('https://ellup-server.onrender.com/')
-const posthere = document.getElementById('postHere')
-window.addEventListener('load',()=>{
-    var today=new Date(); //Today's Date
-    document.getElementById('year').textContent=today.getFullYear()
-    socket.emit('open', '')
-})
-socket.on('posts',(data)=>{
-    
-    var ht = ''
-    data.sort().reverse()
-    for (let i = 0; i < data.length; i++) {     
-       ht += '<div class="col-lg-4 "><div class="card bg-transparent m-1 border border-primary colored_card" style="width:22rem;"><img src="'+data[i].image+'" class="card-img-top img-responsive"/><div class=""><h3>'+data[i].text+'</h3></div><a class="btn btn-sm btn-primary text-white" href="'+data[i].url+'" target="_blank" onclick="countCLick(\''+data[i].id+'\')">Click Here</a></div></div>'
-    }
-   posthere.innerHTML=ht
-})
-function countCLick(id){
-    const data = {
-        id:id
-    }
-    socket.emit('count', data)
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
 }
